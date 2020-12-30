24ebfa308c3916b9fa5cdc1065b7cce9d9019aef
Catherine Dean <catherinemdean15@gmail.com>
2020-12-30 12:04:00 -0700

    add transaction spec with validations

---
+++ spec/models/transaction_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe Transaction, type: :model do
+  describe 'validations' do
+    it {should validate_presence_of :date}
+    it {should validate_presence_of :account}
+    it {should validate_presence_of :type}
+    it {should validate_presence_of :description}
+
+
+  end
+
+end
